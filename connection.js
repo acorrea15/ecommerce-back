76327217cb05ea9b09085975ff48cc035e7c8f6d
Alfredo <acorreacen2@gmail.com>
2023-01-18 00:57:09 -0300

    cambio str de conexión

--- connection.js
+++ connection.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionStr = "Your mongoose connection string";
+/* const connectionStr = "Your mongoose connection string"; */
+const connectionStr = "mongodb+srv://adolcor:Peron2531@cluster0.nngzipz.mongodb.net/?retryWrites=true&w=majority";
+
 
 mongoose.connect(connectionStr, {useNewUrlparser: true})
 .then(() => console.log('connected to mongodb'))
