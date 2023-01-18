474d49db2658c85da011e447d8d28f90e6e5fec7
Alfredo <acorreacen2@gmail.com>
2023-01-18 00:58:33 -0300

    cambio str de conexión 2

--- connection.js
+++ connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 /* const connectionStr = "Your mongoose connection string"; */
+/**/
 const connectionStr = "mongodb+srv://adolcor:Peron2531@cluster0.nngzipz.mongodb.net/?retryWrites=true&w=majority";
 
 
