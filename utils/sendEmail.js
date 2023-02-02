0edf88abe8c0f88a50289cb90dd70e962443723e
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-02-01 21:18:16 -0300

    Rama lab conflictos dev

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: process.env.EMAIL_HOST, 
     port: "587",
     auth: {
       user: process.env.EMAIL_USER,
