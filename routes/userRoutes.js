39ca380d8b90d7e6159affe599387eeec4d5c7f3
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-02-16 19:24:26 -0300

    Agregamos JWT para el login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Order = require('../models/Order');
-// signup
+const jwt = require("jsonwebtoken");
+var ls = require('local-storage');
+const TOKEN_KEY = "x4TvnErxRETbVcqaLl5dqMI115eNlp5y"; 
 
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  console.log(authHeader, "Antes del split");
+  const token = authHeader && authHeader.split(' ')[1];
+  console.log(authHeader, "Después del split");
+  console.log(token, "TOKEN!!!!!!!!!!!!")
+  if(token==null)
+      return res.status(401).send("Token requerido!");
+  jwt.verify(token, TOKEN_KEY, (err, user)=>{
+      if(err) return res.status(403).send("Token inválido!");
+      req.user = user;
+      next();
+  });
+}
+
+// signup
 router.post('/signup', async(req, res)=> {
   const {name, email, password} = req.body;
 
@@ -15,23 +33,41 @@ router.post('/signup', async(req, res)=> {
   }
 })
 
-// login
 
+// login
 router.post('/login', async(req, res) => {
   const {email, password} = req.body;
   try {
-    const user = await User.findByCredentials(email, password);
-    console.log(user,user.isEnabled, "<<<---user")
+    const user = await User.findByCredentials(email, password);   
     if(!user.isEnabled) return res.status(400).send('Usuario no habilitado!');
-    res.json(user)
+    
+    const token = jwt.sign(
+      {userId:user._id, 
+       userName: user.name,
+       userEmail: user.email},
+      TOKEN_KEY, 
+      {expiresIn: "1h"}
+    );
+
+    let nDatos = {...user };
+    nDatos = {...nDatos._doc, token};    
+
+    //Setting localStorage Item
+    ls.set('token_ecommerce5i', token)     
+    console.log(ls.get('token_ecommerce5i'), "<<<--token en local storage-->>>")
+
+
+    /* console.log(nDatos) */
+    res.status(200).json(nDatos);
+
   } catch (e) {
     res.status(400).send(e.message)
   }
 })
 
-// get users;
 
-router.get('/', async(req, res)=> {
+// get users;
+router.get('/', verifyToken, async(req, res)=> {
   try {
     const users = await User.find({ isAdmin: false }).populate('orders');
     res.json(users);
@@ -40,8 +76,8 @@ router.get('/', async(req, res)=> {
   }
 })
 
-// get user orders
 
+// get user orders
 router.get('/:id/orders', async (req, res)=> {
   const {id} = req.params;
   try {
@@ -51,6 +87,8 @@ router.get('/:id/orders', async (req, res)=> {
     res.status(400).send(e.message);
   }
 })
+
+
 // update user notifcations
 router.post('/:id/updateNotifications', async(req, res)=> {
   const {id} = req.params;
